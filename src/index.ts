627d3064aa339aaef8d227736b0a12c2e6c3583f
ritiksharmarj <theonlyritik@gmail.com>
2025-07-02 18:05:03 +0530

    feat: set up Express server with basic routes for home and shortcode

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello India");
+import express from "express";
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.send("hello India 🇮🇳");
+});
+
+app.get("/:shortCode", (req, res) => {
+  const { shortCode } = req.params;
+
+  res.status(200).send(shortCode);
+});
+
+// Create a server on 127.0.0.1:8000
+const port = 8000;
+app.listen(port, () => {
+  console.log(`Server started ${port} 🖐`);
+});
