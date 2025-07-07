d44dfaa82ba0ce8a1e2e479397b678c2a9c4a8a3
ritiksharmarj <theonlyritik@gmail.com>
2025-07-07 20:36:04 +0530

    chore: update PORT configuration in environment file

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ app.use((req, _res, next) => {
 app.use(globalErrorHandler);
 
 // Create a server on 127.0.0.1:8000
-const port = 8000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server started ${port} 🖐`);
 });
