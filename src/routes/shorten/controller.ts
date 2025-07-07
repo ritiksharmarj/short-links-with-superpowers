d73fd1a8448f9e6bb2393d509f39e4542e9d3e86
ritiksharmarj <theonlyritik@gmail.com>
2025-07-08 00:59:30 +0530

    test

--- src/routes/shorten/controller.ts
+++ src/routes/shorten/controller.ts
@@ -17,6 +17,8 @@ export const createShortUrl = catchAsync(
     const validatedData = createShortenSchema.parse(req.body);
     const { url, shortCode } = validatedData;
 
+    console.log("success");
+
     const updatedShortCode = shortCode ?? nanoid(10);
 
     const [result] = await db
@@ -26,6 +28,8 @@ export const createShortUrl = catchAsync(
         shortCode: updatedShortCode,
       })
       .returning();
+
+    console.log(result);
 
     if (!result) {
       return next(new AppError("Failed to create short URL", 400));
